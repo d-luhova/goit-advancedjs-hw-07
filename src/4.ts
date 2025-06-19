85ecf39a4472df7b16b33994aafb5a0d07228c36
Daria Luhova <missis.lugowaya@gmail.com>
2025-06-19 13:06:37 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random()
+    } 
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+      this.key = key;
+    }
+    getKey(): Key {
+      return this.key;
+    }
+  }
+
+  abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);}
+      return;
+    }
+    abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+        return;
+    }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
